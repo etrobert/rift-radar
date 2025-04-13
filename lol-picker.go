a4335ae47beca1459294dd2d891b4705eac9f853
Etienne Robert <etiennerobert33@gmail.com>
2025-04-13 13:00:55 +0200

    create fetch functions

--- lol-picker.go
+++ lol-picker.go
@@ -18,63 +18,85 @@ type Account struct {
 
 var baseUrl = "https://europe.api.riotgames.com/"
 
-func main() {
-	// Load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+var client = &http.Client{}
 
-	apiKey := os.Getenv("RIOT_API_KEY")
-
+func FetchAccount(gameName, tagLine string) (Account, error) {
 	summonerURL := baseUrl + "riot/account/v1/accounts/by-riot-id/Crapow/EUW"
+
+	var apiKey = os.Getenv("RIOT_API_KEY")
 
 	// Create a request with Riot API key header
 	req, _ := http.NewRequest("GET", summonerURL, nil)
 	req.Header.Add("X-Riot-Token", apiKey)
 
-	client := &http.Client{}
 	resp, err := client.Do(req)
 
 	if err != nil {
-		log.Fatal(err)
+		return Account{}, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return Account{}, fmt.Errorf("error: %s", resp.Status)
 	}
 
 	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Error reading response:", err)
+		return Account{}, err
 	}
 
 	// Decode JSON into struct
 	var account Account
 	if err := json.Unmarshal(body, &account); err != nil {
-		log.Fatal("Error decoding JSON:", err)
+		return Account{}, err
 	}
 
-	fmt.Printf("%+v\n", account)
+	return account, nil
+}
 
-	matchesURL := baseUrl + "lol/match/v5/matches/by-puuid/" + account.PUUID + "/ids"
+func FetchMatches(puuid string) ([]string, error) {
+	matchesURL := baseUrl + "lol/match/v5/matches/by-puuid/" + puuid + "/ids"
 
-	req, _ = http.NewRequest("GET", matchesURL, nil)
+	var apiKey = os.Getenv("RIOT_API_KEY")
+	req, _ := http.NewRequest("GET", matchesURL, nil)
 	req.Header.Add("X-Riot-Token", apiKey)
 
-	resp, err = client.Do(req)
+	resp, err := client.Do(req)
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	body, err = io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Error reading response:", err)
+		return nil, err
 	}
 
 	var matches []string
 	if err := json.Unmarshal(body, &matches); err != nil {
-		log.Fatal("Error decoding JSON:", err)
+		return nil, err
 	}
 
-	fmt.Printf("%+v\n", matches)
+	return matches, nil
+}
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+}
+
+func main() {
+	account, err := FetchAccount("Crapow", "EUW")
+
+	if err != nil {
+		log.Fatal("Error fetching account:", err)
+	}
+
+	fmt.Printf("%+v\n", account)
+
+	matches, err := FetchMatches(account.PUUID)
+
+	fmt.Printf("%+v\n", matches)
 }
