1fef39f6b42267fc16928502c4b396c5874b2770
Etienne Robert <etiennerobert33@gmail.com>
2025-06-03 21:19:56 +0200

    replace placeholder with centered grid: Team 1, Suggestions, Team 2

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div className="grid place-items-center grid-cols-[auto_1fr_auto]">
+      <div>Team 1</div>
+      <div>Suggestions</div>
+      <div>Team 2</div>
+    </div>
+  );
 }
 
 export default App;
